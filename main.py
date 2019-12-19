76dcb7df64c18357889c0ded91e39e070ac6d2b1
ReyVZ <andreydenary@gmail.com>
2019-12-19 21:07:40 +0300

    20191219

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import cherrypy
+import pandas as pd
+
+
+class KKT(object):
+    @cherrypy.expose
+    def index(self):
+        return 'index'
+
+    @cherrypy.expose
+    def dictionary(self):
+        return open('kkt.html', 'r')
+
+
+@cherrypy.expose
+@cherrypy.tools.json_out()
+class DictGenerator(object):
+    @cherrypy.tools.accept(media='application/json')
+    def GET(self):
+        data = pd.read_html('Dictionary.html')[0]
+        data.columns = ['key', 'description', 'default', 'type', 'aggr', 'doc']
+        data['id'] = list(range(len(data)))
+        data['aggr'] = data['aggr'].apply(
+            lambda x: True if x == 'Возможно' else False)
+        return data.to_dict(orient='records')
+
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/generator': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'application/json')]
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    app = KKT()
+    app.generator = DictGenerator()
+    cherrypy.quickstart(app, '/', conf)
---
+++ public/js/script.js
@@ -0,0 +1,96 @@
+res = [];
+output = "";
+scan_form = function () {
+    el = document.forms.doc.elements
+    types = "[";
+    for (e in el) {
+        if (el[e].checked == true) {
+            types += el[e].name.slice(3) + ", ";
+        }
+    }
+    types = types.length > 1 ? types.slice(0, types.length - 2) + "]" : "[]";
+    return types
+
+}
+add_tag = function (id) {
+    btn = document.getElementById("button" + id);
+    if (btn.innerHTML == "Добавить") {
+        fn = document.getElementById("select" + id);
+        el = document.getElementById("tag" + id);
+        el.classList.add("added");
+        btn.innerHTML = "Удалить";
+        if (fn != null && fn.value != "---") {
+            res.push(fn.value + "(" + id + ")");
+        } else {
+            res.push(id);
+        }
+        if (fn != null) {
+            fn.setAttribute("disabled", "");
+        }
+        reset();
+    }
+    else {
+        fn = document.getElementById("select" + id);
+        el = document.getElementById("tag" + id);
+        el.classList.remove("added");
+        btn.innerHTML = "Добавить";
+        if (fn != null && fn.value != "---") {
+            res.splice(res.indexOf(fn.value + "(" + id + ")"), 1);
+        } else {
+            res.splice(res.indexOf(id), 1);
+        }
+        if (fn != null) {
+            fn.removeAttribute("disabled");
+        }
+        reset();
+    }
+}
+reset = function () {
+    output = "{\n\t\"filter\":{\n\t\t\"forDocumentTypes\": " + scan_form() + "\n\t},\n\t\"output\": [\n";
+    for (r in res) {
+        if (isNaN(res[r])) {
+            s = res[r];
+            id = parseInt(s.slice(s.indexOf("(") + 1, s.indexOf(")")));
+            fn = s.slice(0, s.indexOf("("));
+            d = data.filter(function (x) { return x["id"] == id })[0];
+            output += "\t\t{ \"key\": \"" + d.key + "\", \"DisplayName\": \"" + d.description +
+                "\", \"function\": \"" + fn + "\" },\n";
+        } else {
+            d = data.filter(function (x) { return x["id"] == res[r] })[0];
+            output += "\t\t{ \"key\": \"" + d.key + "\", \"DisplayName\": \"" + d.description + "\" },\n";
+        }
+    }
+    output = output.slice(0, output.length - 2) + "\n\t]\n}";
+    document.getElementById("output").innerHTML = output;
+}
+xhr = new XMLHttpRequest();
+xhr.open('GET', '/generator', false);
+xhr.send();
+data = JSON.parse(xhr.responseText);
+table = "";
+for (d in data) {
+    table += "<tr id=\"tag" + data[d].id + "\">\
+            <td>" + data[d].id + "</td>\
+            <td>" + data[d].key + "</td>\
+            <td>" + data[d].description + "</td>\
+            <td>" + data[d].doc + "</td>\
+            <td>" + (data[d].aggr == true ?
+            "<select id=\"select" + data[d].id + "\">\
+                    <option value=\"---\">---</option>\
+                    <option value=\"sum\">sum</option>\
+                    <option value=\"avg\">avg</option>\
+                </select>": "") + "</td>\
+            <td><button id=\"button" + data[d].id + "\" onclick=\"add_tag(" + data[d].id + ")\">Добавить</button></td>\
+            </tr>"
+}
+tags = document.getElementById("tags");
+tags.innerHTML = table;
+doc_types = [1, 11, 2, 3, 31, 301, 4, 41, 401, 5];
+form = "";
+for (t in doc_types) {
+    i = doc_types[t];
+    form += "<label for=\"doc" + i + "\">" + i + "</label>" +
+        "<input type=\"checkbox\" name=\"doc" + i + "\" id=\"doc" + i + "\">&nbsp&nbsp"
+}
+doc = document.forms.doc;
+doc.innerHTML = form;
